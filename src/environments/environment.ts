b8883661db2e66ba38e321c036ea5b73889d2806
Ricardo Mejía <ricardo.mejia@ejemplo.com>
2025-11-01 20:51:01 -0600

    Primer commit - auth-login (Angular session manager)

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-auth-9d51a',
+    appId: '1:55331776450:web:326963b1a84f519b60585b',
+    storageBucket: 'angular-auth-9d51a.appspot.com',
+    apiKey: 'AIzaSyCekSAvjzHxKWsXgBmkjvQoLlrho_Dow7E',
+    authDomain: 'angular-auth-9d51a.firebaseapp.com',
+    messagingSenderId: '55331776450',
+    measurementId: 'G-71S2XCSBCP',
+  },
+};
